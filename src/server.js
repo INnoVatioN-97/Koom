996f1953e02c935823b267288bfc2a4cfdf8f0bd
INnoVatioN-97 <koyoungil97@gmail.com>
2021-09-14 20:44:30 +0900

    브라우저간 메시지 전송, 백-프론트간 메시지 전송 구현

--- src/server.js
+++ src/server.js
@@ -1,12 +1,35 @@
 import express from 'express';
+import WebSocket from 'ws';
+import http from 'http';
 
 const app = express();
 
 app.set('view engine', 'pug');
 app.set('views', __dirname + '/views');
 app.use('/public', express.static(__dirname + '/public'));
-app.get('/', (req, res) => res.render('home'));
-app.get('/*', (req, res) => res.redirect('/'));
+
+app.get('/', (_, res) => res.render('home'));
+// 위 지정하지 않은 다른 url로 접근하려할 때 자동으로 /로 리다이렉트
+app.get('/*', (_, res) => res.redirect('/'));
 
 const handleListen = () => console.log('Listening on http://localhost:3000');
-app.listen(3000, handleListen);
+// app.listen(3000, handleListen);
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+const sockets = [];
+
+wss.on('connection', (socket) => {
+    sockets.push(socket);
+    console.log('Connected to Browser');
+    socket.on('close', () => {
+        console.log('Disconnected from client');
+    });
+    socket.on('message', (msg) => {
+        const message = msg.toString('utf-8');
+        sockets.forEach((aSocket) => aSocket.send(message));
+    });
+});
+
+server.listen(3000, handleListen);
